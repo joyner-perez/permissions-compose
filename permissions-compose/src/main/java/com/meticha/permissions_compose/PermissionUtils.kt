04ce4e1fbb60b2a1050ebd51ab50ab4d3aa699a6
Joyner Pérez Echevarria <joynerechevarria@gmail.com>
2026-02-19 15:43:55 +0100

    chore: improve permission check for Android Tiramisu

--- permissions-compose/src/main/java/com/meticha/permissions_compose/PermissionUtils.kt
+++ permissions-compose/src/main/java/com/meticha/permissions_compose/PermissionUtils.kt
@@ -2,19 +2,25 @@ package com.meticha.permissions_compose
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import android.util.Log
 import kotlin.collections.contains
 
 internal fun checkPermissionAddedInManifest(
     permission: AppPermission,
     context: Context
-): Boolean =
-    try {
-        val packageInfo =
+): Boolean = try {
+        val packageInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            context.packageManager.getPackageInfo(
+                context.packageName,
+                PackageManager.PackageInfoFlags.of(PackageManager.GET_PERMISSIONS.toLong())
+            )
+        } else {
             context.packageManager.getPackageInfo(
                 context.packageName,
                 PackageManager.GET_PERMISSIONS
             )
+        }
         val declaredPermissions = packageInfo.requestedPermissions ?: emptyArray()
         declaredPermissions.contains(permission.permission)
     } catch (e: Exception) {
